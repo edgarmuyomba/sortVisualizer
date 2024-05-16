3550cd6a0755ac41d705b04f095ee8980a61300a
edgarmuyomba <edgarmatthew247@gmail.com>
2024-05-16 15:22:09 +0300

    display window and context api setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,41 @@
 import React from 'react';
+
+import { createContext } from 'react';
+
 import './App.scss'
 import Sidebar from './components/sidebar/sidebar';
+import AnimationWindow from './components/animationWindow/animationWindow';
+
+interface Settings {
+  speed: number;
+  count: number;
+  algorithm: 'bubblesort' | 'insertionsort' | 'mergesort' | 'quicksort' | 'selectionsort';
+  array: number[];
+  setSpeed: () => void;
+  setCount: () => void;
+  generateArray: () => void;
+  shuffleArray: () => void;
+  sortArray: () => void;
+}
+
+export const SettingsContext = createContext<Settings>({
+  speed: 1,
+  count: 10,
+  algorithm: 'bubblesort',
+  array: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+  setSpeed: () => { },
+  setCount: () => { },
+  generateArray: () => { },
+  shuffleArray: () => { },
+  sortArray: () => { },
+})
 
 function App() {
 
   return (
     <>
       <Sidebar />
+      <AnimationWindow />
     </>
   )
 }
---
+++ src/components/animationWindow/animationWindow.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+import styles from "./styles.module.scss";
+
+export default function AnimationWindow() {
+
+    const [array, setArray] = useState<number[]>([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+    function getHeight(int: number): string {
+        return `${int * 2.5}rem`
+    }
+
+    return (
+        <div className={styles.container}>
+            {
+                array.map((int: number, index: number) => {
+                    return (
+                        <div key={index} className={styles.element} style={{ height: getHeight(int) }}>
+                            <p className={styles.value}>
+                                {int}
+                            </p>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
